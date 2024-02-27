f0c073302ddf449ad052783e7cb6a9983bcc5fc5
YonghwanYim <uko991531@gmail.com>
2024-02-27 17:50:36 +0900

    add scaler

--- run_cpu.py
+++ run_cpu.py
@@ -9,7 +9,7 @@ class RunCPU():
         config.read(config_path)
         # number of dataset (1, 2, 3, 4)
         self.num_dataset = int(config['SimulationSettings']['num_dataset'])
-        # The boundary value of unit numbers for dividing the train and test datasets
+        # The boundary value of unit numbers for dividing the train and valid datasets
         self.split_unit_number = int(config['SimulationSettings']['split_unit_number'])
         # Number of sample datasets
         self.num_sample_datasets = int(config['SimulationSettings']['num_sample_datasets'])
@@ -21,22 +21,37 @@ class RunCPU():
         # class instance 생성
         self.env = SimulationEnvironment()
         # dataset 분할
-        self.train_data, self.test_data, self.full_data = self.env.load_data(self.num_dataset, self.split_unit_number)
+        self.train_data, self.valid_data, self.full_data = self.env.load_data(self.num_dataset, self.split_unit_number)
         # sampling
         self.sampled_datasets = self.env.sampling_datasets(self.num_sample_datasets, self.observation_probability,
-                                                      self.train_data, self.test_data, self.full_data)
+                                                      self.train_data, self.valid_data, self.full_data)
         # sampled_datasets에 RUL column 추가.
         self.sampled_datasets_with_RUL = self.env.add_RUL_column_to_sampled_datasets(self.sampled_datasets)
 
     def run(self, data_sample_index):
         print('run')
         train_data = self.sampled_datasets_with_RUL[data_sample_index][0].copy()
-        test_data = self.sampled_datasets_with_RUL[data_sample_index][1].copy()
+        valid_data = self.sampled_datasets_with_RUL[data_sample_index][1].copy()
         full_data = self.sampled_datasets_with_RUL[data_sample_index][2].copy()
+
+
+        # Data preprocessing 1 : separate index, label, data
+        train_index_names, x_train, x_test, y_train, y_test = self.env.drop_labels_from_train_data(train_data)
+        valid_index_names, x_valid, y_valid = self.env.drop_labels_from_data(valid_data)
+        full_index_names, x_full, y_full = self.env.drop_labels_from_data(full_data)
+
+        ## Data preprocessing 2 : scaling
+        x_train = self.env.data_scaler(x_train)
+        x_valid = self.env.data_scaler(x_valid)
+        x_full = self.env.data_scaler(x_full)
+
+
+
 
     def run_many(self):
         # run()을 data sample만큼 반복수행.
         # 나중에 코드 추가하자.
+        # 여기서 sample 수 만큼 실행하고, 평균을 낸 후 파일로 저장하는 것 까지는 다뤄주면 좋음.
         print("test")
 
 
@@ -45,6 +60,7 @@ class RunCPU():
 
 runCPU = RunCPU('config1.ini')
 # test
-runCPU.run_many()
+runCPU.run(0)
+
 
 
--- simulation_env.py
+++ simulation_env.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 import sklearn
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 import random
 
 # tensorflow.keras 오류 수정하자. loss 5,6 학습할 때 필요함.
@@ -19,6 +19,7 @@ class SimulationEnvironment():
     def __init__(self):
         self.index_names = ['unit_number', 'time_cycles']
         self.setting_names = ['setting_1', 'setting_2', 'setting_3']
+        self.drop_lables = self.index_names + self.setting_names
         self.sensor_names = ['s_{}'.format(i + 1) for i in range(0, 21)]
         self.col_names = self.index_names + self.setting_names + self.sensor_names
         self.dataset_paths = {
@@ -36,14 +37,14 @@ class SimulationEnvironment():
         dataset_path = self.dataset_paths[dataset_number]
         data = pd.read_csv(dataset_path, sep='\s+', header=None, index_col=False, names=self.col_names)
 
-        # dataset의 unit_number를 기준으로 train, test, full data로 나누기
-        train = data[data['unit_number'] <= split_number] # train data (fitting)
-        test = data[data['unit_number'] > split_number]   # test data
-        full = data.copy()                                # full data
+        # dataset의 unit_number를 기준으로 train, valid, full data로 나누기
+        train = data[data['unit_number'] <= split_number]  # train data (fitting)
+        valid = data[data['unit_number'] > split_number]   # valid data
+        full = data.copy()                                 # full data
 
-        return train, test, full
+        return train, valid, full
 
-    def sampling_datasets(self, num_sample_datasets, observation_probability, train, test, full):
+    def sampling_datasets(self, num_sample_datasets, observation_probability, train, valid, full):
         # 샘플링 결과를 저장할 리스트 초기화
         sampled_data_list = []
 
@@ -51,16 +52,16 @@ class SimulationEnvironment():
         for i in range(num_sample_datasets):    # i means random seed
             # 샘플링 수행
             sampled_train = train.sample(frac=observation_probability, random_state=i)
-            sampled_test = test.sample(frac=observation_probability, random_state=i)
+            sampled_valid = valid.sample(frac=observation_probability, random_state=i)
             sampled_full = full.sample(frac=observation_probability, random_state=i)
 
             # Sorting
             sampled_train = sampled_train.sort_values(by=['unit_number', 'time_cycles'])
-            sampled_test = sampled_test.sort_values(by=['unit_number', 'time_cycles'])
+            sampled_valid = sampled_valid.sort_values(by=['unit_number', 'time_cycles'])
             sampled_full = sampled_full.sort_values(by=['unit_number', 'time_cycles'])
 
             # 샘플링 결과를 리스트에 추가
-            sampled_data_list.append((sampled_train.copy(), sampled_test.copy(), sampled_full.copy()))
+            sampled_data_list.append((sampled_train.copy(), sampled_valid.copy(), sampled_full.copy()))
 
         return sampled_data_list
 
@@ -77,15 +78,40 @@ class SimulationEnvironment():
         sampled_data_with_RUL_list = []
 
         # 각각의 샘플링 데이터셋에 RUL 열 추가
-        for sampled_train, sampled_test, sampled_full in sampled_datasets:
+        for sampled_train, sampled_valid, sampled_full in sampled_datasets:
             sampled_train_with_RUL = self.add_RUL_column(sampled_train)
-            sampled_test_with_RUL = self.add_RUL_column(sampled_test)
+            sampled_valid_with_RUL = self.add_RUL_column(sampled_valid)
             sampled_full_with_RUL = self.add_RUL_column(sampled_full)
 
             # 결과를 리스트에 추가
-            sampled_data_with_RUL_list.append((sampled_train_with_RUL, sampled_test_with_RUL, sampled_full_with_RUL))
+            sampled_data_with_RUL_list.append((sampled_train_with_RUL, sampled_valid_with_RUL, sampled_full_with_RUL))
 
         return sampled_data_with_RUL_list
+
+    def drop_labels_from_train_data(self, train_data): # for train data
+        train_dataset_index_names = train_data[['unit_number', 'time_cycles']]   # save index name (for scaling)
+        dropped_train_dataset = train_data.drop(columns=self.drop_lables).copy() # split dataset
+        tmp_x_train, tmp_x_test, tmp_y_train, tmp_y_test = train_test_split(dropped_train_dataset, dropped_train_dataset['RUL'], test_size=0.01, shuffle=False)
+
+        return train_dataset_index_names, tmp_x_train, tmp_x_test, tmp_y_train, tmp_y_test
+
+    def drop_labels_from_data(self, data): # for valid, full data
+        dataset_index_names = data[['unit_number', 'time_cycles']]   # save index name (for scaling)
+        dropped_dataset = data.drop(columns=self.drop_lables).copy() # split dataset
+        labels_data = data[['RUL']]                                  # save true RUL
+
+        return dataset_index_names, dropped_dataset, labels_data
+
+    def data_scaler(self, data):
+        scaler = MinMaxScaler()  # MinMax Scaler
+        # Droping the target variable (True RUL)
+        data.drop(columns=['RUL'], inplace=True)  # inplace -> true면 원본 삭제 (current option)  # inplace -> False면 원본은 유지
+        scaled_data = scaler.fit_transform(data)
+
+        return scaled_data
+
+
+
 
 
 
