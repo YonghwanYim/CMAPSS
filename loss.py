8402be6e5580e0d94b94035018afaac33ba83cde
Yonghwan Yim <uko991531@gmail.com>
2025-05-29 16:18:23 +0900

    Add a comment about asymmetric penalties for over/under predictions

--- loss.py
+++ loss.py
@@ -1,10 +1,13 @@
-# Directed Mean Squared Error를 고려한 Loss function
 import tensorflow as tf
 
 previous_prediction = None
 previous_true_label = None
 
 def directed_mse_loss(y_true, y_pred):
+    """
+    Apply asymmetric penalties for over- and under-predictions
+    by incorporating prediction direction into the loss function.
+    """
     penalty = tf.where(y_pred > y_true, 13.0, 10.0)
     squared_error = tf.square(y_pred - y_true)
     return tf.reduce_mean(penalty * squared_error, axis=-1)
